c7ee5d8a32431d56edf5bacd03b7004e7c77f579
rewrite404 <aaronlin1996@gmail.com>
2019-06-19 17:17:11 +0800

    ()

--- uart.py
+++ uart.py
@@ -110,9 +110,9 @@ if __name__ == '__main__':
 
         try:
             print('Hello world')
-            t2 = threading.Thread(target=reboot_count())
+            t2 = threading.Thread(target=reboot_count)
             t2.start()
-            t1 = threading.Thread(target=read_uart())
+            t1 = threading.Thread(target=read_uart)
             print('t2 started')
             print('t1 started')
             t1.start()
